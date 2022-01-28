6acf7d1b24ed22e379784947842f6aee1f4c4bc7
blackarea <supersute142@gmail.com>
2022-01-28 18:12:10 +0900

    홈페이지에 이름 나오게 구현

--- src/main/java/com/graduation/parrot/controller/BoardController.java
+++ src/main/java/com/graduation/parrot/controller/BoardController.java
@@ -4,25 +4,30 @@ import com.graduation.parrot.config.SecurityUser;
 import com.graduation.parrot.domain.User;
 import com.graduation.parrot.domain.form.BoardForm;
 import com.graduation.parrot.domain.form.BoardResponseForm;
-import com.graduation.parrot.domain.form.BoardSaveForm;
-import com.graduation.parrot.repository.UserRepository;
 import com.graduation.parrot.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
-    private final UserRepository userRepository;
+    private final HttpSession httpSession;
 
     @GetMapping({"/", "/board"})
-    public String index(Model model){
-
+    public String index(Model model, @AuthenticationPrincipal SecurityUser principal){
+        if(principal != null){
+            model.addAttribute("userName", principal.getUser().getName());
+        }
         model.addAttribute("boardList", boardService.getBoardList());
         return "index";
     }
