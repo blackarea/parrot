85c2ab0f0f5f63bf4ea7c7d4f07463f4b6a90cba
blackarea <supersute142@gmail.com>
2022-02-03 21:25:21 +0900

    테스트 환경 맞춤

--- src/main/java/com/graduation/parrot/controller/HomeController.java
+++ src/main/java/com/graduation/parrot/controller/HomeController.java
@@ -2,24 +2,22 @@ package com.graduation.parrot.controller;
 
 import com.graduation.parrot.domain.Board;
 import com.graduation.parrot.domain.User;
-import com.graduation.parrot.domain.form.BoardSaveForm;
 import com.graduation.parrot.repository.BoardRepository;
 import com.graduation.parrot.repository.UserRepository;
-import com.graduation.parrot.service.BoardService;
-import com.graduation.parrot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
 import java.util.stream.IntStream;
 
+@Profile("base")
 @Controller
 public class HomeController {
 
     @Autowired private UserRepository userRepository;
     @Autowired private BoardRepository boardRepository;
+
 
     @PostConstruct
     public void createUserAndBoard(){
