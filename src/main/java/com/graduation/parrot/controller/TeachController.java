fcd44a9d980bc4092908804f734f5640d0ca48ea
blackarea <supersute142@gmail.com>
2022-03-24 16:55:45 +0900

    Teach 페이지 연결완료

--- src/main/java/com/graduation/parrot/controller/TeachController.java
+++ src/main/java/com/graduation/parrot/controller/TeachController.java
@@ -1,7 +1,28 @@
 package com.graduation.parrot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TeachController {
+
+    @GetMapping("/teach")
+    public String teach(){
+        return "teach/teach";
+    }
+
+    @GetMapping("/teach/polar")
+    public String teachPolar(){
+        return "teach/teachPolar";
+    }
+
+    @GetMapping("/teach/free")
+    public String teachFree(){
+        return "teach/teachFree";
+    }
+
+    @GetMapping("/mission")
+    public String mission(){
+        return "teach/mission";
+    }
 }
