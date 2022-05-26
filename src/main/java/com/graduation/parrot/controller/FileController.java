50e2944021474ff0ae31fa3d0300a5a350c54060
blackarea <supersute142@gmail.com>
2022-05-26 16:21:07 +0900

    이미지 전송 시스템 추가

--- src/main/java/com/graduation/parrot/controller/FileController.java
+++ src/main/java/com/graduation/parrot/controller/FileController.java
@@ -2,6 +2,7 @@ package com.graduation.parrot.controller;
 
 import com.graduation.parrot.domain.dto.FileDto;
 import com.graduation.parrot.service.FileService;
+import com.graduation.parrot.webSocket.WebSocketService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import java.io.IOException;
 public class FileController {
 
     private final FileService fileService;
+    private WebSocketService webSocketService = new WebSocketService();
 
     @PostMapping("/file")
     public FileDto storeFile(@RequestParam("file")MultipartFile multipartFile) throws IOException {
@@ -27,5 +29,13 @@ public class FileController {
         FileDto fileDto = fileService.saveFile(multipartFile);
         log.info("fileDto = {}", fileDto);
         return fileDto;
+    }
+
+    @PostMapping("/file/sendimage")
+    public void sendImage(MultipartFile multipartFile) throws IOException, InterruptedException {
+        if(multipartFile.isEmpty()){
+            return;
+        }
+        webSocketService.sendImage(multipartFile, "ws://localhost:8888/ws/image");
     }
 }
--- src/main/java/com/graduation/parrot/webSocket/WebSocketService.java
+++ src/main/java/com/graduation/parrot/webSocket/WebSocketService.java
@@ -2,8 +2,13 @@ package com.graduation.parrot.webSocket;
 
 import lombok.extern.slf4j.Slf4j;
 import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.util.Base64;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 
 @Slf4j
 public class WebSocketService {
@@ -32,4 +37,25 @@ public class WebSocketService {
         webSocketUtil.close();
         return pythonMessage;
     }
+
+    public void sendImage(MultipartFile multipartFile, String url) throws InterruptedException, IOException {
+        WebSocketUtil webSocketUtil = new WebSocketUtil(URI.create(url), new Draft_6455());
+        webSocketUtil.connectBlocking();
+
+        File file = convertToFile(multipartFile);
+        byte[] bytes = Files.readAllBytes(file.toPath());
+
+        String s = Base64.encodeBytes(bytes);
+        // ByteBuffer 를 클라이언트로 보낸다.
+        webSocketUtil.send(s);
+        webSocketUtil.run();
+        webSocketUtil.close();
+    }
+
+    public File convertToFile(MultipartFile multipartFile) throws IOException {
+        File file = new File(multipartFile.getOriginalFilename());
+        multipartFile.transferTo(file);
+        return file;
+    }
+
 }
